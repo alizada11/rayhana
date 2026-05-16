11f85f1ce8fc8c393e88727bb0687b42a9793902
alizada11 <jawadalizada1@gmail.com>
2026-05-16 20:39:08 +0330

    SEOBugFixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -157,7 +157,7 @@ function App({ ssrPath, ssrSearch }: AppProps) {
   useUserSync();
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
+      <ThemeProvider defaultTheme="dark">
         <ConfirmProvider>
           <TooltipProvider>
             <Suspense fallback={null}>
